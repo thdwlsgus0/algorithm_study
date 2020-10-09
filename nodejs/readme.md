ef0caa71d0def46e941803b2d8c64522c9620785
thdwlsgus0 <thdwlsgus0@naver.com>
2020-10-09 18:11:11 +0900

    문제 풀었던 내용

---
+++ nodejs/readme.md
@@ -0,0 +1,9 @@
+### node.js 알고리즘
+
+---
+
+#### nodejs를 이용해서 알고리즘 문제를 풉니다.
+
+---
+
+- 프로그래머스 https://programmers.co.kr/learn/courses/30/lessons/12917 문자열 내림차순 정렬하기
---
+++ nodejs/문자열내림차순정렬하기.js
@@ -0,0 +1,11 @@
+// 프로그래머스 https://programmers.co.kr/learn/courses/30/lessons/12917
+function solution(s) {
+  return s
+    .split("")
+    .sort((a, b) => {
+      if (a > b) return -1;
+      if (b > a) return 1;
+      return 0;
+    })
+    .join("");
+}
