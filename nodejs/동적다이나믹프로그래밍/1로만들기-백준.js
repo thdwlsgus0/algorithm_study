d03430156aca5bbbad9d1863cee87e6ee4695e88
thdwlsgus0 <thdwlsgus0@naver.com>
2021-02-28 22:41:58 +0900

    1로만들기 추가

---
+++ nodejs/동적다이나믹프로그래밍/1로만들기-백준.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const n = +input[0];
+const dp = Array(30001).fill(0);
+
+for(let i=2; i<=n; i++){
+  dp[i] = dp[i-1]+1;
+  if(i%2===0){
+    dp[i] = Math.min(dp[i], dp[Math.floor(i/2)]+1);
+  }
+  if(i%3===0){
+    dp[i] = Math.min(dp[i], dp[Math.floor(i/3)]+1);
+  }
+}
+console.log(dp[n]);
